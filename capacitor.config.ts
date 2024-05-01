e70683a614c0121bd385a8704a65d5e6150c9281
xiaoqie <438518174@qq.com>
2024-05-01 12:49:48 +0800

    feat: 添加Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ai.xiaoben.reader",
+  appName: "xiaoben-reader",
+  webDir: "out",
+};
+
+export default config;
