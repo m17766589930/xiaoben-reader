bc0cf7d80b724a7c8c60691c91daf86f3fabf6af
申柯秋 <438518174@qq.com>
2024-05-01 23:04:07 +0800

    feat: 新增SSR配置

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,11 @@ const config: CapacitorConfig = {
   appId: "ai.xiaoben.reader",
   appName: "xiaoben-reader",
   webDir: "out",
+  server: {
+    hostname: "xiaoben-reader-lfqmwci4k-m17766589930s-projects.vercel.app/",
+    iosScheme: "https",
+    androidScheme: "https"
+  }
 };
 
 export default config;
